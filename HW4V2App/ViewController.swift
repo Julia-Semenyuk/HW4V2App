752ea2d32892177d1da677a493010d149120750e
Julia Semenyuk <julia_semenyuk@internet.ru>
2022-08-07 01:28:28 +0900

    1

--- HW4V2App/ViewController.swift
+++ HW4V2App/ViewController.swift
@@ -9,11 +9,89 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    
+    @IBOutlet var redLabelNumber: UILabel!
+    @IBOutlet var greenLabelNumber: UILabel!
+    @IBOutlet var blueLabelNumber: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 20
+        setupSlider()
+        setupLabel()
+        
     }
-
-
+    
+    @IBAction func redSliderAction() {
+        redLabelNumber.text = String(format: "%.2f", redSlider.value)
+        changeColor()
+    }
+    
+    
+    @IBAction func greenSliderAction() {
+        greenLabelNumber.text = String(format: "%.2f",greenSlider.value)
+        changeColor()
+    }
+    
+    
+    @IBAction func blueSliderAction() {
+        blueLabelNumber.text = String(format: "%.2f",blueSlider.value)
+        changeColor()
+    }
+    
+// MARK: - Private Methods
+    private func setupLabel() {
+        redLabel.text = "Red:"
+        redLabel.textColor = .white
+        redLabelNumber.text = String(format: "%.2f", redSlider.value)
+        redLabelNumber.textColor = .white
+        
+        greenLabel.text = "Green:"
+        greenLabel.textColor = .white
+        greenLabelNumber.text = String(format: "%.2f",greenSlider.value)
+        greenLabelNumber.textColor = .white
+        
+        blueLabel.text = "Blue:"
+        blueLabel.textColor = .white
+        blueLabelNumber.text = String(format: "%.2f",blueSlider.value)
+        blueLabelNumber.textColor = .white
+    }
+    
+    private func setupSlider() {
+        redSlider.value = 0
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 100
+        redSlider.minimumTrackTintColor = .red
+        
+        greenSlider.value = 0
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 100
+        greenSlider.minimumTrackTintColor = .green
+        
+        blueSlider.value = 0
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 100
+        blueSlider.minimumTrackTintColor = .blue
+    }
+    
+    private func changeColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                            green: CGFloat(greenSlider.value),
+                                            blue: CGFloat(blueSlider.value),
+                                            alpha: 1)
+    }
 }
 
